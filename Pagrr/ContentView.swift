3b793271448f4547db62b4d14116625caba1c316
Valentin Lehmann <info@valentinlehmann.de>
2026-01-01 18:32:39 +0100

    Initial Commit

---
+++ Pagrr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pagrr
+//
+//  Created by Valentin Lehmann on 01.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pagrr/PagrrApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagrrApp.swift
+//  Pagrr
+//
+//  Created by Valentin Lehmann on 01.01.26.
+//
+
+import SwiftUI
+
+@main
+struct PagrrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
